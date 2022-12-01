755fa06d9574a823e1104efc22ee6f0f33612f4c
unknown <giorgi.sharvadze.1@btu.edu.ge>
2022-12-01 20:22:27 +0400

    first commi

---
+++ aopspp/resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<div class="col-md-10 col-md-offset-1">
+      <input id="name" type="text" name="name" />
+</div>
+<div class="col-md-10 col-md-offset-1">
+      <input  type="submit" value="submit" />
+</div>
